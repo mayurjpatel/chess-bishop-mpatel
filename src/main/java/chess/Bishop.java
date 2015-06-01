58623f6a7e8de0ceae3c3651e3e29cdf690c981a
Mayur Patel <mayur.j.patel@gmail.com>
2015-05-31 20:50:46 -0400

    Implemented the Bishop Next Positions routine. Also, added basic unit tests for it.

--- src/main/java/chess/Bishop.java
+++ src/main/java/chess/Bishop.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -11,6 +12,37 @@ public class Bishop {
      * This method should retrieve the possible positions of a piece on a chess board.
      */
     public Set<Position> getNextPositions(Position origin) {
-        return null;
+        Set<Position> retVal = new HashSet<Position>();
+        int curRow = origin.getRow();
+        int curCol = (int)(origin.getColumn());
+        int maxColInt = (int)(Position.MAX_COLUMN);
+        int minColInt = (int)(Position.MIN_COLUMN);
+
+        // if cur row = 4, cur col = d aka 100
+        for (int i = curRow+1, j=curCol+1; i <= Position.MAX_ROW && j <= maxColInt; i++, j++) {
+            // i = { 5, 6, 7, 8 }
+            // j = { e, f, g, h }
+            // pos = { e5, f6, g7, h8 }
+            retVal.add(new Position((char) j, i));
+        }
+        for (int i = curRow+1, j=curCol-1; i <= Position.MAX_ROW && j >= minColInt; i++, j--) {
+            // i = { 5, 6, 7, 8 }
+            // j = { c, b, a}
+            // pos = { c5, b6, a7 }
+            retVal.add(new Position((char) j, i));
+        }
+        for (int i = curRow-1, j=curCol+1; i >= Position.MIN_ROW && j <= maxColInt; i--, j++) {
+            // i = { 1, 2, 3 }
+            // j = { e, f, g, h }
+            // pos = { e1, f2, g3 }
+            retVal.add(new Position((char) j, i));
+        }
+        for (int i = curRow-1, j=curCol-1; i >= Position.MIN_ROW && j >= minColInt; i--, j--) {
+            // i = { 1, 2, 3 }
+            // j = { a, b, c }
+            // pos = { a1, b2, c3 }
+            retVal.add(new Position((char) j, i));
+        }
+        return retVal;
     }
 }
--- src/test/java/chess/BishopTest.java
+++ src/test/java/chess/BishopTest.java
@@ -3,6 +3,8 @@ package chess;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+
 import static junit.framework.Assert.*;
 
 /**
@@ -11,17 +13,64 @@ import static junit.framework.Assert.*;
 public class BishopTest {
 
     private Bishop bishop;
-    private Position start;
-
+    private static final Map<String, String> testPositions = new HashMap<String, String>();
+    
     @Before
     public void setUp() {
         bishop = new Bishop();
-        start = new Position("d4");
+        //add a middle position
+        testPositions.put("d4", "@1a,@2b,@3c,@5e,@6f,@7g,@8h,@7a,@6b,@5c,@3e,@2f,@1g");
+        //add corners
+        testPositions.put("a1", "@2b,@3c,@4d,@5e,@6f,@7g,@8h");
+        testPositions.put("a8", "@7b,@6c,@5d,@4e,@3f,@2g,@1h");
+        testPositions.put("h1", "@8a,@7b,@6c,@5d,@4e,@3f,@2g");
+        testPositions.put("h8", "@1a,@2b,@3c,@4d,@5e,@6f,@7g");
+        //add a pos on the edge
+        testPositions.put("e1", "@2f,@3g,@4h,@2d,@3c,@4b,@5a");
+        testPositions.put("a3", "@2b,@1c,@4b,@5c,@6d,@7e,@8f");
+        testPositions.put("f8", "@7g,@6h,@7e,@6d,@5c,@4b,@3a");
+        testPositions.put("h4", "@3g,@2f,@1e,@5g,@6f,@7e,@8d");
     }
 
+    /**
+     * Test whether we get not null results for this position
+     */
     @Test
     public void testNextPositionsNotNull() throws Exception {
-        assertNotNull("The set of next positions can never be null", bishop.getNextPositions(start));
+        for (String pos : testPositions.keySet()) {
+            Position position = new Position(pos);
+            assertNotNull("The set of next positions for " + position + " can never be null",
+                    bishop.getNextPositions(position));
+        }
+    }
+
+    /**
+     * Test whether positions returned match with expected positions
+     * @throws Exception
+     */
+    @Test
+    public void testValuesNextPosition() throws Exception {
+        for (String pos : testPositions.keySet()) {
+            Position position = new Position(pos);
+            Set<String> expectedPositions = computeExpectedPositions(testPositions.get(pos));
+            Set<Position> nextPos = bishop.getNextPositions(position);
+            assertEquals("Checking expected position size for " + pos, expectedPositions.size(), nextPos.size());
+            for (Position p : nextPos) {
+                assertTrue("Checking whether a given pos: " + p.toString() + " exist in result for start pos: " + pos,
+                        expectedPositions.contains(p.toString()));
+            }
+        }
+    }
+
+    /**
+     * A helper function which converted a string of comma separated position strings to a set of strings
+     * @param expectedPosString: A string of comma separated position strings. example: "a2,b3,c4"
+     * @return expectedPosSet: A set of strings
+     */
+    private Set<String> computeExpectedPositions(String expectedPosString) {
+        Set<String> retval = new LinkedHashSet<String>();
+        retval.addAll(Arrays.asList(expectedPosString.split(",")));
+        return retval;
     }
 
 }
